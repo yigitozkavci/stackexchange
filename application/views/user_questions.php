f3c6b781ce9e7283e6d0c7781d645b1fc198f63d
esra-aydemir <h.esraaydemir@hotmail.com>
2015-03-12 17:51:11 +0200

    Bug Fixes

--- application/views/user_questions.php
+++ application/views/user_questions.php
@@ -6,8 +6,9 @@
 			$track++;
 			?>
 		<div class="panel panel-default">
+			<div data-toggle="collapse" data-parent="#accordion" href="#collapse-<?php echo $track; ?>">
 			    <div class="panel-heading">
-			    	<div data-toggle="collapse" data-parent="#accordion" href="#collapse-<?php echo $track; ?>">
+			    	
 		 		   	    <h4 class="panel-title">
 		             		<span><?php echo $question['title']; ?></span>
 		        		</h4>
